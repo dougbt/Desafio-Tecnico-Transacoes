1e8c085fdb0bc6db51d307b842816945ae7161f1
dougbt <douglasbt.dev@gmail.com>
2025-08-03 14:09:47 -0300

    feat: ajustes no Strategy Factory

--- src/main/java/br/com/ngbilling/DesafioTecnico/domain/service/strategy/TransacaoStrategyFactory.java
+++ src/main/java/br/com/ngbilling/DesafioTecnico/domain/service/strategy/TransacaoStrategyFactory.java
@@ -2,27 +2,25 @@ package br.com.ngbilling.DesafioTecnico.domain.service.strategy;
 
 import br.com.ngbilling.DesafioTecnico.domain.model.FormaDePagamento;
 import br.com.ngbilling.DesafioTecnico.domain.service.TransacaoStrategy;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.EnumMap;
 import java.util.Map;
 
-@Component
+@Configuration
 public class TransacaoStrategyFactory {
-    private final Map<FormaDePagamento, TransacaoStrategy> strategies;
 
-    public TransacaoStrategyFactory(
+    @Bean
+    public Map<FormaDePagamento, TransacaoStrategy> transacaoStrategyMap(
             CreditoStrategy creditoStrategy,
             DebitoStrategy debitoStrategy,
             PixStrategy pixStrategy) {
 
-        this.strategies = new EnumMap<>(FormaDePagamento.class);
-        this.strategies.put(FormaDePagamento.CREDITO, creditoStrategy);
-        this.strategies.put(FormaDePagamento.DEBITO, debitoStrategy);
-        this.strategies.put(FormaDePagamento.PIX, pixStrategy);
-    }
-
-    public TransacaoStrategy getStrategy(FormaDePagamento formaPagamento) {
-        return strategies.get(formaPagamento);
+        Map<FormaDePagamento, TransacaoStrategy> strategies = new EnumMap<>(FormaDePagamento.class);
+        strategies.put(FormaDePagamento.CREDITO, creditoStrategy);
+        strategies.put(FormaDePagamento.DEBITO, debitoStrategy);
+        strategies.put(FormaDePagamento.PIX, pixStrategy);
+        return strategies;
     }
 }
